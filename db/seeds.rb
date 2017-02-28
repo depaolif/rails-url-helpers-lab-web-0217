fb156360988cf7317064dc6ba5bc8f405281629a
Frederico De Paoli <frederico.de.paoli@gmail.com>
2017-02-28 17:17:43 -0500

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+frederico = Student.create(first_name:"Frederico", last_name: "De Paoli")
